17a26002853bc1589340b7de32550e864e6355e0
samuel-ctrl <samsaras2002@gmail.com>
2023-09-05 18:59:21 +0530

    bug fixed after navigate scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/landingPage/LandingPage";
 import ContactPage from "./Pages/contact/ContactPage";
+import { ScrollToTop } from "./components/Utils";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="contact/" element={<ContactPage />}></Route>
